f180288346820a68164150f36d91b39b0f3fd441
abdel <holbertonc21@gmail.com>
2023-06-16 02:35:00 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+function that return 0 if success
+*/
 int main (void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
