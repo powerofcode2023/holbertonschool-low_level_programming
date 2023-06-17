a1367319051434411d8dd3acf1ad5e8b5ed0654e
abdel <holbertonc21@gmail.com>
2023-06-17 11:56:57 -0700

    commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'print string'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("int size: %ld bytes\n", sizeof(int));
+printf("float size: %ld bytes\n", sizeof(float));
+printf("char size: %ld bytes\n", sizeof(char));
+printf("short size: %ld bytes\n", sizeof(short)); 
+return (0);
+}
