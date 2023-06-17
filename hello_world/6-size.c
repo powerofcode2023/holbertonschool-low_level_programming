308a0b2fd2d688c0761b55c86dfc78496d6fc925
abdel <holbertonc21@gmail.com>
2023-06-17 12:04:38 -0700

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-  printf("Size of char: %ld bytes\n", sizeof(char));
+printf("Size of char: %ld bytes\n", sizeof(char));
 printf("Size of int: %ld bytes\n", sizeof(int));
 printf("Size of long int: %ld bytes\n", sizeof(long int));
 printf("Size of long long int: %ld bytes\n", sizeof(long long int));
