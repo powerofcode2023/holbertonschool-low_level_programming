e8bf2b654e793a4409d6427e4dea4d81cd2af340
abdel <holbertonc21@gmail.com>
2023-06-19 10:11:55 -0700

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 mean success
+ */
+int main(void)
+{
+char alphabet;
+char alphabet2;
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+putchar(alphabet);
+putchar('\n');
+else if (alphabet2 = 'A' ; alphabet2 <= 'Z' ; alphabet2++)
+putchar(alphabet2);
+putchar('\n');
+return (0);
+}
