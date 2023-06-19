3b4d45624aba97aadd21d3c735504cc242865735
abdel <holbertonc21@gmail.com>
2023-06-19 11:02:59 -0700

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,13 @@ int main(void)
 {
 char alphabet;
 for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
 if (alphabet == 'q' || alphabet == 'e')
 {
 continue;
 }
 putchar(alphabet);
+}
 putchar('\n');
 return (0);
 }
