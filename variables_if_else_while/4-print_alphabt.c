5e46bcf2c93abfed182a9f495fb61072ffeb104c
abdel <holbertonc21@gmail.com>
2023-06-19 10:58:31 -0700

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 mean success
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+if (alphabet = 'q' || alphabet = 'e')
+{
+continue;
+}
+putchar(alphabet);
+putchar('\n');
+return (0);
+}
