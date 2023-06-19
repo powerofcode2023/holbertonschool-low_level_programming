72c65feee9713686c81aa57dce9e54b355d8933b
abdel <holbertonc21@gmail.com>
2023-06-19 11:38:23 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 mean success
+ */
+int main(void)
+{
+int numbers;
+for (numbers = 0 ; numbers <= 10 ; numbers++)
+{
+printf("Single digit numbers are %d", numbers);
+}
+putchar("\n");
+return (0);
+}
