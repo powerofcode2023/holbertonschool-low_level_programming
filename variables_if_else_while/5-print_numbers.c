f37ff95fd0f5aeb753f64ffdae085c8eccc66949
abdel <holbertonc21@gmail.com>
2023-06-19 11:40:24 -0700

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,7 @@ int main(void)
 int numbers;
 for (numbers = 0 ; numbers <= 10 ; numbers++)
 {
-printf("Single digit numbers are %d", numbers);
+printf(" %d", numbers);
 }
 putchar('\n');
 return (0);
