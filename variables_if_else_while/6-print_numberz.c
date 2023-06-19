e34c85c7250749b421eab501e3bd87e77e0a7b88
abdel <holbertonc21@gmail.com>
2023-06-19 12:48:38 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 mean success
+ */
+int main(void)
+{
+int numbers;
+for (numbers = '0' ; numbers <= '9' ; numbers++)
+{
+putchar(numbers);
+}
+putchar('\n');
+return (0);
+}
