f71786b9fb6a48ee35b0a2fe22717759b34ab879
abdel <holbertonc21@gmail.com>
2023-06-19 10:14:31 -0700

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,14 @@ int main(void)
 char alphabet;
 char alphabet2;
 for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+{
 putchar(alphabet);
 putchar('\n');
+}
 else if (alphabet2 = 'A' ; alphabet2 <= 'Z' ; alphabet2++)
+{
 putchar(alphabet2);
 putchar('\n');
+}
 return (0);
 }
