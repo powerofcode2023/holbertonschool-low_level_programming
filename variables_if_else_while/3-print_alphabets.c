aa2001010b878df8a6cfc515cd376f6737323f21
abdel <holbertonc21@gmail.com>
2023-06-19 10:16:00 -0700

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -14,7 +14,7 @@ for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 putchar(alphabet);
 putchar('\n');
 }
-else if (alphabet2 = 'A' ; alphabet2 <= 'Z' ; alphabet2++)
+for (alphabet2 = 'A' ; alphabet2 <= 'Z' ; alphabet2++)
 {
 putchar(alphabet2);
 putchar('\n');
