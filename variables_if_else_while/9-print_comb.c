c7b379f41caccea0ed6f7454528aeb18f2e2084c
abdel <holbertonc21@gmail.com>
2023-06-20 01:22:55 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 mean success
+ */
+int main(void)
+{
+  char combinations;
+  for (combinations = '0' ; combinations <= '9' ; combinations++)
+    {
+      putchar(combinations);
+      putchar(', ');
+    }
+  putchar('\n');
+  return '0);
+}
