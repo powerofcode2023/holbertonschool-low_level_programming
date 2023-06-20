680f58fb0db3014a548628bb426f33bf893dce55
abdel <holbertonc21@gmail.com>
2023-06-20 07:20:09 -0700

    general commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-int main (void)
+int main(void)
 {
   _putchar('_');
   _putchar('p');
@@ -11,6 +11,7 @@ int main (void)
   _putchar('h');
   _putchar('a');
   _putchar('r');
+  _putchar('\n');
 
   return (0);
 }
