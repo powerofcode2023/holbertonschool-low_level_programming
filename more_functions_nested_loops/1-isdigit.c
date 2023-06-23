5ae921fbbc73467ee73ca620c528b3605bea9b5b
abdel <holbertonc21@gmail.com>
2023-06-23 11:13:21 -0700

    commit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -5,15 +5,11 @@
  * _isdigit - check the digit
  *
  * Return:  1 or 0.
- 
+ */ 
 int _isdigit(int c);
 {
-if (c >= '0' && c <= '9')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+  if (c >= '0' && c <= '9')
+    return (1);
+  else
+    return (0);
 }
