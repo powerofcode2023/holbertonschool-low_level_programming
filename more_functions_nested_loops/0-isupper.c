02edde835ff3aec6eb4cefc6257eb0e1e0849d98
abdel <holbertonc21@gmail.com>
2023-06-23 09:26:26 -0700

    0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * _isupper - if it is uppercase
+ *
+ * Return: 0 or 1.
+ */
+int _isupper(int c)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
