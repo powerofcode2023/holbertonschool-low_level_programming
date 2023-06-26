811d27396a8eadf1766973bdd396ae5a10c4b9d1
abdel <holbertonc21@gmail.com>
2023-06-26 06:53:58 -0700

    6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,21 @@
 #include "main.h"
+#include "2-strlen.c"
 /**
- *
- *
- *
+ * puts2 - prints every other character
+ * @str: string
  */
 
 void puts2(char *str)
 {
-  int i;
+	int i;
 
-  for (i = 0; i < _strlen(str); i++)
-    {
-      if (i % 2 == 0)
+	i = 0;
+
+	while (str[i] != '\0')
 	{
-	  _putchar(str[i]);
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
-    }
-  _putchar('\n');
+	_putchar('\n');
 }
