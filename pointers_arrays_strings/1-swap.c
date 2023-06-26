0b9c7dff45c35f63554c27110c07a4bc16fed09f
abdel <holbertonc21@gmail.com>
2023-06-26 03:12:22 -0700

    1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,26 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
-void invert_num(int *pt_num1, int *pt_num2)
-{
-    int temp = 0;
-
-    temp = *pt_num2;
-    *pt_num2 = *pt_num1;
-    *pt_num1 = temp;
-}
 
 void swap_int(int *numA, int *numB);
 {
-    int numA = 15;
-    int numB = 28;
+  int temp;
 
-    int *pointernumA = &numA;
-    int *pointernumB = &numB;
-
-    printf("AVANT: A= %d et B = %d\n", numA, numB);
-    invert_num(pointernumA, pointernumB);
-    printf("APRES: A= %d et B = %d\n", numA, numB);
-
-    return (0);
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }
