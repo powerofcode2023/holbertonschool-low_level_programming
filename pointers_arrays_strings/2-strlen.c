4331c3fa212aee4a547ecd41a5b54d5f77d75e75
abdel <holbertonc21@gmail.com>
2023-06-26 05:12:21 -0700

    2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - return length of a string
+ *@s: string
+ *Return: length of the string
+ */
+
+
+int _strlen(char *s);
+{
+  i= 0;
+  while (*s !='0')
+    {
+      s++;
+      i++;
+    }
+  return(i);
+}
