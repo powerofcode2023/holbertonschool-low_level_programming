5b23afb310dc650ca9e6826f9610c96ee0d63bb5
abdel <holbertonc21@gmail.com>
2023-06-28 07:36:28 -0700

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -13,12 +13,12 @@ int dest_len = 0;
 int i;
 
 while (dest[dest_len] != '\0')
-        dest_len++;
-   
-for (i = 0; src[i] != '\0'; i++)
-        dest[dest_len++] = src[i];
+dest_len++;
 
-    
+for (i = 0; src[i] != '\0'; i++)
+dest[dest_len++] = src[i];
+
+
 dest[dest_len] = '\0';
 
 return (dest);
