02999a737728f56c9225ed09dad500c4cf619737
abdel <holbertonc21@gmail.com>
2023-06-28 07:20:09 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] == s2[i])
+	while (*s1 && *s2)
 	{
-		if (s1[i] == '\0')
-		  return (0);
-		i++;
+		if (*s1 - *s2 != 0)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-
-	return (s1[i] - s2[i]);
+	return (0);
 }
