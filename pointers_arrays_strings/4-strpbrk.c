aa45a7061caa376d5fcf83a4de54e4686ccd61e8
abdel <holbertonc21@gmail.com>
2023-06-30 02:09:56 -0700

    4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,22 +2,22 @@
 
 /**
  * _strpbrk - locates a character in a string
- * @s: pointer where we sarch for character
- * @accept: character we search
+ * @s: pointer where we search for charachter
+ * @accept: character we search for
  * Return: NULL if character is not found, return pointer
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-  char *s1 = s - 1;
-  int i;
+	char *s1 = s - 1;
+	int i;
 
-  do {
-    s1++;
-    for (i=0; *(accept +1) != '\0'; i++)
-      if (*s1 == *(accept + 1))
-	return (s1);
-  } while (*s1 != '\0');
+	do {
+		s1++;
+		for (i = 0; *(accept + i) != '\0'; i++)
+			if (*s1 == *(accept + i))
+				return (s1);
+	} while (*s1 != '\0');
 
-  return (0);
+	return (0);
 }
