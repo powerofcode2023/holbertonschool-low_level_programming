0fd326184b13843ef93b8f154ffd3025f3d450ea
abdel <holbertonc21@gmail.com>
2023-07-04 03:13:53 -0700

    3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,22 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+int factorial(int n)
 {
-    int r;
-
-    r = factorial(1);
-    printf("%d\n", r);
-    r = factorial(5);
-    printf("%d\n", r);
-    r = factorial(10);
-    printf("%d\n", r);
-    r = factorial(-1024);
-    printf("%d\n", r);
-    return (0);
+  if( n < 0)
+    return (-1);
+  if( n == 0 )
+    return (1);
+  return (n * factorial(n - 1));
 }
+  
