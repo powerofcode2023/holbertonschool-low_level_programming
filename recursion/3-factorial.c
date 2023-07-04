1fb357c7d1405eafbf5ddd61ec324c8ccca3126a
abdel <holbertonc21@gmail.com>
2023-07-04 03:23:05 -0700

    3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- factorial - factorize n
+ **factorial - factorize n
  *@n - integer
  *
  * Return: sucess 1
@@ -9,9 +9,9 @@
 
 int factorial(int n)
 {
-if(n < 0)
+if (n < 0)
 return (-1);
-if(n == 0)
+if (n == 0)
 return (1);
 return (n * factorial(n - 1));
-}  
+}
