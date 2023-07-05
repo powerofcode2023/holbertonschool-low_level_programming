079988e3a8e967cc752d2fca8cc9cf46269599d1
abdel <holbertonc21@gmail.com>
2023-07-05 06:00:12 -0700

    general

---
+++ static_libraries/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int dest_len = 0;
+int i;
+
+while (dest[dest_len] != '\0')
+dest_len++;
+
+for (i = 0; src[i] != '\0'; i++)
+dest[dest_len++] = src[i];
+
+
+dest[dest_len] = '\0';
+
+return (dest);
+}
