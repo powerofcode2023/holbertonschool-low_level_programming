ea514d05e21ce689fc3104155cf37b9838711cb0
abdel <holbertonc21@gmail.com>
2023-07-07 02:47:15 -0700

    0-whatsmyname.c fix

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,15 +7,9 @@
  */
 
 
-int  main(int argc, char *argv[])   
+int  main(__attribute((unused))int argc, char *argv[])   
 {
-    int i;
-    for (i = 0; i < argc; i++)
-    {
-        
-        printf("%s ", argv[0]);       
-    }
-    printf("\n");
+    printf("%s\n ", argv[0]);       
     return (0);
 }
 
