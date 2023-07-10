4fb331854eae4cc064f3ef22e84cde216eecc35f
abdel <holbertonc21@gmail.com>
2023-07-10 05:27:58 -0700

    fix betty

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	pointer = malloc(sizeof(c) * size);
 	
-	if (pointer == NULL) 
+	if (pointer == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
