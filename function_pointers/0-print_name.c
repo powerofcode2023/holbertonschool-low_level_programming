36ab5b65688785878b8243f314ff903ed54c2d7d
abdel <holbertonc21@gmail.com>
2023-07-20 01:06:49 -0700

    0-print_name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: a pointer to a function that print a name
  */
 
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
