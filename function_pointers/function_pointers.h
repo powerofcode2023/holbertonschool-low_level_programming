b4fec2283c809612d69d43cf24b8680f7d92aa96
abdel <holbertonc21@gmail.com>
2023-07-20 00:56:42 -0700

    functions pointers h

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTIONS_POINTERS_H
+#define FUNCTIONS_POINTERS_H
+#include <stdio.h>
+/**
+ * File: functions_pointers.h
+ * description: header file containing prototypes
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
